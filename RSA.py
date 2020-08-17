5bec1ec2a356360eef84d3105a16fe01f43dce9b
Giiltham <68121091+Giiltham@users.noreply.github.com>
2020-08-17 02:49:29 +0200

    Add files via upload

--- RSA.py
+++ RSA.py
@@ -5,9 +5,9 @@ class RSA:
         self.keys = ()
         self.publicKey = []
         self.privateKey = []
-    def get_new_key(self):
-        self.bits = int(input("how many bits ? "))
-        self.keys = rsa.newkeys(self.bits)
+        
+    def getNewKey(self, bits):
+        self.keys = rsa.newkeys(bits)
         self.publicKey = self.keys[0]
         self.privateKey = self.keys[1]
         return self.keys
@@ -17,24 +17,19 @@ class RSA:
     def getPrivateKey(self):
         return "rsa." + str(self.privateKey)
     
-    def selectPublicKey(self):
-        self.key = input("enter your PublicKey (rsa.PublicKey(....)): ")
-        self.publicKey = eval(self.key)
-    def selectPrivateKey(self):
-        self.key = input("enter your PrivateKey (rsa.PrivateKey(....)): ")
-        self.privateKey = eval(self.key)
+    def selectPublicKey(self, key):
+        self.publicKey = eval(key)
+    def selectPrivateKey(self, key):
+        self.privateKey = eval(key)
 
-    def encrypt(self):
-        self.message = input("enter your message: ")
+    def encrypt(self, message):
+        message = message.encode("utf-8")
         
-        self.message = self.message.encode("utf-8")
-        
-        self.EncryptedMessage = str(rsa.encrypt(self.message, self.publicKey))[2:-1]
+        self.EncryptedMessage = str(rsa.encrypt(message, self.publicKey))[2:-1]
         
         return self.EncryptedMessage
-    
-    def decrypt(self):
-        self.message = bytes(input("enter your encrypted message: "),'ISO-8859-1').decode('unicode_escape').encode('ISO-8859-1')
-        self.DecryptedMessage = rsa.decrypt(self.message, self.privateKey)
+    def decrypt(self, message):
+        message = bytes(message,'ISO-8859-1').decode('unicode_escape').encode('ISO-8859-1')
+        self.DecryptedMessage = rsa.decrypt(message, self.privateKey)
         return str(self.DecryptedMessage)[2:-1]
     
--- RSA_Interface.py
+++ RSA_Interface.py
@@ -5,32 +5,45 @@ rsa = importlib.import_module("RSA")
 RSA_instance = rsa.RSA()
 
 def help():
-    print("get_new_key \ndecrypt \nencrypt \nselectPublicKey \nselectPrivateKey \ngetPublicKey \ngetPrivateKey")    
+    print("getNewKey \ndecrypt \nencrypt \nselectPublicKey \nselectPrivateKey \ngetPublicKey \ngetPrivateKey")    
 
 
 while(True):
     command = input("Command > ")    
     
-    if command == "get_new_key":
-        keys = RSA_instance.get_new_key()
+    if command == "getNewKey":
+        bits = int(input("how many bits ? "))
+        keys = RSA_instance.getNewKey(bits)
         print("\nPublicKey: rsa." + str(keys[0]) + "\n")
         print("PrivateKey: rsa." + str(keys[1]) +"\n")
+        
     elif command == "decrypt":
-        print(RSA_instance.decrypt())
+        message = input("enter your encrypted message: ")
+        print(RSA_instance.decrypt(message))
+        
     elif command == "encrypt":
-        print(RSA_instance.encrypt())
+        message = input("enter your message: ")
+        print(RSA_instance.encrypt(message))
+        
     elif command == "getPublicKey":
         print(RSA_instance.getPublicKey())
+        
     elif command == "getPrivateKey":
         print(RSA_instance.getPrivateKey())
+        
     elif command == "selectPublicKey":
-        RSA_instance.selectPublicKey()
+        key = input("enter your PublicKey (rsa.PublicKey(....)): ")
+        RSA_instance.selectPublicKey(key)
         if input("do you want to encrypt ? (Y/N): ").capitalize() == "Y":
-            print(RSA_instance.encrypt())
+            message = input("enter your message: ")
+            print(RSA_instance.encrypt(message))
+        
     elif command == "selectPrivateKey":
-        RSA_instance.selectPrivateKey()
+        key = input("enter your PrivateKey (rsa.PrivateKey(....)): ")
+        RSA_instance.selectPrivateKey(key)
         if input("do you want to decrypt ? (Y/N): ").capitalize() == "Y":
-            print(RSA_instance.decrypt())
+            message = input("enter your encrypted message: ")
+            print(RSA_instance.decrypt(message))
             
     elif command == "help":
         help()
